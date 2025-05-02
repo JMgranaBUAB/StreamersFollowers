15318e77c465ed09980f67f9b0033e8daaa0fcd1
jmgranabuab <josemiguel@quesada.eu>
2025-05-02 09:44:01 +0200

    fix: build project

--- src/TopStreamersCountik.js
+++ src/TopStreamersCountik.js
@@ -1,40 +1,40 @@
 import React from 'react';
 
 const TikTokCounter = ({ username }) => (
-  <div>
-    <iframe
-      src={`https://livecounts.io/embed/tiktok-live-follower-counter/${username}`}
-      title={`TikTok Live Follower Counter - ${username}`}
-      style={{ border: "0", width: "300px", height: "80px" }}
-    ></iframe>
-  </div>
+    <div>
+        <iframe
+            src={`https://livecounts.io/embed/tiktok-live-follower-counter/${username}`}
+            title={`TikTok Live Follower Counter - ${username}`}
+            style={{ border: "0", width: "300px", height: "80px" }}
+        ></iframe>
+    </div>
 );
 
 const TopStreamersCountik = () => {
-  const streamers = [
-    "mrbeast",
-    "ibaillanos",
-    "thegrefg",
-    "rubius",
-    "elxokas",
-    "auronplay",
-    "iamcristinini",
-    "zyrpesland",
-    "illojuan",
-    "knekro_",
-    "lamejorclaseyt",
-    "puertaracing",
-    "alexbyonce",
-    "jmgranabuab"
-  ];
+    const streamers = [
+        "mrbeast",
+        "ibaillanos",
+        "thegrefg",
+        "rubius",
+        "elxokas",
+        "auronplay",
+        "iamcristinini",
+        "zyrpesland",
+        "illojuan",
+        "knekro_",
+        "lamejorclaseyt",
+        "puertaracing",
+        "alexbyonce",
+        "jmgranabuab"
+    ];
 
-  return (
-    <>
-      {streamers.map((username) => (
-        <TikTokCounter key={username} username={username} />
-      ))}
-    </>
-  );
+    return (
+        <>
+            {streamers.map((username) => (
+                <TikTokCounter key={username} username={username} />
+            ))}
+        </>
+    );
 };
 
 export default TopStreamersCountik;
