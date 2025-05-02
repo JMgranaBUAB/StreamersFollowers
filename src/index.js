624aaa7847b36096fd3e324ca5a85a261ffba97a
jmgranabuab <josemiguel@quesada.eu>
2025-05-02 09:03:01 +0200

    feat: add streamers to https://livecounts.io/

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HomePage from './HomePage';
+import TopStreamersCountik from './TopStreamersCountik';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <HomePage />
+    {/* <HomePage /> */}
+    <TopStreamersCountik />
   </React.StrictMode>
 );
 
